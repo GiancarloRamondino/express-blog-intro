6e5e8981567a78330c1534bc7927a15bcd0457ac
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-04-16 22:50:20 +0200

    [UPD] Rotta  bacheca

--- app.js
+++ app.js
@@ -2,15 +2,26 @@
 const express = require('express');
 //inizializzo express nella variabile  app
 const app = express();
-//ddeefinisco il numero di porta
+//deefinisco il numero di porta
 const port = 3000;
+
+//imposto asset statici
+app.use(express.static('public'));
 
 //definisco la rotta principale
 app.get('/', (req, res) => {
   res.send('Server del mio blog!');
 });
 
+//definisco la rotta per il blog
+app.get('/bacheca', (req, res) => {
+  res.send('Benvenuto nel mio blog!');
+});
+
 //server che ascolta sulla porta 3000
 app.listen(port, () => {
-  console.log(`Server in ascolto su http://localhost:${port}`);
+  console.log(`Server in ascolto su ${port}`);
+   
 });
+
+console.log('Server in ascolto su 3000');
