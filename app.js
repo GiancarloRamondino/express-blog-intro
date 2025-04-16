1a1dfad82849fe67f08ef67cb6e9cde1e8c68f06
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-04-16 22:27:55 +0200

    [FIX] package

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+//importo expreess
 const express = require('express');
+//inizializzo express nella variabile  app
 const app = express();
+//ddeefinisco il numero di porta
 const port = 3000;
 
+//definisco la rotta principale
 app.get('/', (req, res) => {
   res.send('Server del mio blog!');
 });
 
+//server che ascolta sulla porta 3000
 app.listen(port, () => {
   console.log(`Server in ascolto su http://localhost:${port}`);
 });
